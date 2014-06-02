9cd4d94f016ccaa98a624b281ffb3f9b537ba87d
Hack Reactor Students <maseh87@gmail.com>
2014-06-02 15:06:45 -0700

    refactoring

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,9 +1,28 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
+// This function will return any elements of given `className` within the dom. It is a
+// reimplementation of the native `getElementsByClassName`, intentionally using recursion
+var getElementsByClassName = function(targetClassName) {
 
-// But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
-  // your code here
+  // create a storage element
+  var results = [];
+  // check initial element for class name and if it matches targetClassName, push to storage
+  function search(element) {
+    if(element.className !== undefined) {
+      var classNameList = element.className.split(' ');
+      if(classNameList.indexOf(targetClassName) !== -1) {
+        results.push(element);
+      }
+    }
+  // check if element has children, if so, iterate last two steps for each of them.
+    if (element.childNodes.length > 0) {
+      for (var i = 0; i < element.childNodes.length; i++) {
+        search(element.childNodes[i]);
+      }
+    }
+  };
+
+  search(document.body);
+  // after iterating all children, return storage element
+  return results;
 };
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,42 @@
-// this is what you would do if you liked things to be easy:
+// this is what I might do if I didn't like to learn things:
 // var stringifyJSON = JSON.stringify;
 
-// but you don't so you're going to write it from scratch:
+// but I do, so I'm going to write it from scratch:
+
+// This function receives a value of any type, and returns a stringified version
+// of it analogous to JSON.stringify
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var result;
+	//determine what obj is
+	//if obj is an Array or an object
+	function check(obj) {
+		if(Array.isArray(obj)) {
+			//iterate over obj
+			for(var i = 0; i < obj.length; i++) {
+				check(obj[i]);
+			}
+		}
+
+		//if obj is string handle escape characters of the string
+		else if(typeof obj === 'string') {
+			escapeStrings(obj);
+		}
+
+		//if obj is number or null or boolean return string of obj
+		else if(typeof obj === 'number' || obj === null || typeof obj === 'boolean') {
+			return String(obj);
+		}
+
+		//if obj is undefined return undefined
+		else if(obj === undefined) {
+			return undefined;
+		}
+
+		else {
+			for(var key in obj) {
+				check(obj[key]);
+			}
+		}
+	}
+	return result.join(', ');
 };
